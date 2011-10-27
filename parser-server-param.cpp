4a735adc1ede3246650054820819c38f772240d4
Soroush Rabiei <soroush.rabiei@gmail.com>
2011-10-27 20:38:11 +0330

    Adding parser skeleton for server_param

--- parser-server-param.cpp
+++ parser-server-param.cpp
@@ -1,2 +1,66 @@
 #include "parserbase.h"
 #include <iostream>
+#include <QString>
+
+void ParserBase::sp_read()
+{
+    i++;
+    if(input[i]=='(')
+    {
+        sp_readParameterName();
+    }
+}
+
+void ParserBase::sp_readParameterName()
+{
+    i++;
+    switch(input[i])
+    {
+    case 'a':
+        i++; // skip 'u' in 'au'
+        sp_au();
+        break;
+    case 'b':
+        i++; // skip 'a' in 'ba'
+        sp_ba();
+        break;
+    }
+}
+
+void ParserBase::sp_au()
+{
+    i++;
+    QString value;
+    switch(input[i])
+    {
+    case 'd':
+        i+=12;
+        while(input[i]!=')')
+        {
+            value+=input[i];
+            i++;
+        }
+        std::cout << "audio_cut_dist: " << value.toInt() << std::endl;
+        sp_read();
+        break;
+    case 't':
+        i+=8;
+        std::cout << "auto_mode: " << input[i] << std::endl;
+        i++;
+        sp_read();
+        break;
+    }
+}
+
+void ParserBase::sp_ba()
+{
+    i++;
+    switch(input[i])
+    {
+    case 'c':
+        break;
+    case 'l':
+        break;
+    }
+
+}
