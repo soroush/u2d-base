059a54688914a16004a0fa7d89876bae09332810
soroush <soroush-r@users.sourceforge.net>
2012-08-01 00:50:22 +0430

    main file added. (testing proposed)

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#include "agent.h"
+#include <sstream>
+
+void foo (istream& i)
+{
+
+}
+
+int main()
+{
+    Agent x;
+    x.start();
+    return 0;
+}
+
